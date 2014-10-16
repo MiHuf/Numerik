277d5174cc6eee2ddc1264136b66219228ecd6f6
Michael Hufschmidt <michael@hufschmidt-web.de>
2014-10-16 22:47:46 +0200

    Gerüst für alles Folgende

---
+++ Blatt01_Aufg1.m
@@ -0,0 +1,7 @@
+% Übungen zur Numerik, WS 2014/15
+% Blatt 01, Aufgabe 1
+% Autoren: Janina Geiser, Michael Hufschmidt, Farina Ohm, Annika Seidel
+A = [1 1 ; 1 1.01]
+x = [500 ; 500]
+b = [1000 ; 1005]
+A * x - b
