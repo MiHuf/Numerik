401eafd0c6f5bf7106905f4ee2c0a749a48a5edb
Michael Hufschmidt <michael@hufschmidt-web.de>
2014-10-27 20:50:02 +0100

    Gerüst für Blatt 3

---
+++ Blatt03_Aufg10.m
@@ -0,0 +1,13 @@
+% Übungen zur Numerischen Mathematik, WS 2014/15
+% Blatt 03, Aufgabe 10
+% Autoren: 
+%   Janina Geiser, Mat Nr. 6420269
+%   Michael Hufschmidt, Mat.Nr. 6436122
+%   Farina Ohm, Mat Nr. 6314051
+%   Annika Seidel, Mat Nr. 6420536
+% Übungsleiter:
+%   Thomas Berger <thomas.berger@uni-hamburg.de>
+
+
+clear all
+clc
