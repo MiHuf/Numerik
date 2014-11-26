e47b0fd7a61dd1546bc5c0376e86c0225618f8f1
Michael Hufschmidt <michael@hufschmidt-web.de>
2014-11-26 21:32:23 +0100

    Test plottet jetzt auch

--- test_aufg14.m
+++ test_aufg14.m
@@ -23,28 +23,33 @@ A = 2*diag(ones(1,n))-diag(ones(1,n-1),1)-diag(ones(1,n-1),-1) ;
 b = zeros(n,1) ;
 b(1) = 1 ;
 b(n) = 1 ;
-eps = 1e-12 ;                % Fehlesteps_gv = 0;                % Anzahl Schritte
-steps_gv = 0 ;
-steps_ev = 0 ;
-max_steps = 10000 ;
-
-r_gv = zeros(1, max_steps) ; % Residuen
-r_ev = zeros(1, max_steps) ;
-gv_steps = zeros(1, max_steps) ;
-ev_steps = zeros(1, max_steps) ;
-
+eps = 1e-12 ;                     % Fehler
+size = [2 inf] ;                  % Size der r_ev und r_gv Matrizen
 
 disp('Gesamtschritt-Verfahren: x =')
 xg = gv(A, b, eps) ;
 disp(xg)
+% Einlesen der Daten fuer den Plot
+fi = fopen('gv.dat','r');
+r_gv = fscanf(fi, '%d %e', size) ;
+fclose(fi) ;
+max_gv = max(r_gv(1,:)) ;
 
 disp('Einzelschritt-Verfahren: x =')
 xe = ev(A, b, eps) ;
 disp(xe)
-
-fi = fopen('gv.dat','r');
-% M = textscan(fi, '%d %e');
+% Einlesen der Daten fuer den Plot
+fi = fopen('ev.dat','r');
+r_ev = fscanf(fi, '%d %e', size) ;
 fclose(fi) ;
+max_ev = max(r_ev(1,:)) ;
 
-% gv_steps
-
+% Plotte die Ergebnisse
+semilogy(r_gv(1,1:max_gv), r_gv(2,1:max_gv),  'r-', 'linewidth', 2) ;
+xlabel('Anzahl Schritte', 'FontSize', 12) ;
+ylabel('Residumm, logarithmisch',  'FontSize', 12) ;
+grid on ;
+hold on ;
+semilogy(r_ev(1,1:max_ev), r_ev(2,1:max_ev),  'g-', 'linewidth', 2) ;
+legend('Gesamtschritt-Verfahren', 'Einzelschritt-Verfahren') ;
+title('Einzelschritt-Verfahren konvergiert doppelt so schnell !', 'FontSize', 12, 'FontWeight', 'bold') 
