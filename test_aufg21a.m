d3b1552c1584b7234370898e9ca5968a69f0fadc
Michael Hufschmidt <michael@hufschmidt-web.de>
2014-12-02 20:09:04 +0100

    Aufgabe 21b fertig

--- test_aufg21a.m
+++ test_aufg21a.m
@@ -1,5 +1,5 @@
 % Uebungen zur Numerischen Mathematik, WS 2014/15
-% Blatt 06, Aufgabe 21b
+% Blatt 06, Aufgabe 21a
 %
 % Autoren:
 %   Janina Geiser, Mat Nr. 6420269
@@ -9,15 +9,5 @@
 % Uebungsleiter:
 %   Thomas Berger <thomas.berger@uni-hamburg.de>
 %
-
-function[J] = DF(x)
-% Berechnet die Jacobi-Matrix von F an der Stelle x gemaess Uebungsblatt
-n = size(x) ;
-n = 2 ;
-J = zeros(n,n) ;
-J(1,1) = 2 * x(1) ;
-J(1,2) = 0 ;
-J(2,1) = -1 ;
-J(2,2) = 3 * x(2)^2 ;
-return
-end
+clc
+clear all
