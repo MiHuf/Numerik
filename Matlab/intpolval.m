d5e7b7205d59d8f7f691cf1d4097704f3a72f78c
Michael Hufschmidt <michael@hufschmidt-web.de>
2014-12-14 17:31:52 +0100

    nur Kopf mit Kommentarzeilen ergänzt

--- Matlab/intpolval.m
+++ Matlab/intpolval.m
@@ -1,3 +1,14 @@
+% Uebungen zur Numerischen Mathematik, WS 2014/15
+% Blatt 08, Aufgabe 28
+%
+% Autoren:
+%   Janina Geiser, Mat Nr. 6420269
+%   Michael Hufschmidt, Mat.Nr. 6436122
+%   Farina Ohm, Mat Nr. 6314051
+%   Annika Seidel, Mat Nr. 6420536
+% Uebungsleiter:
+%   Thomas Berger <thomas.berger@uni-hamburg.de>
+%
 function [y] = intpolval(x,D,xst)
 
 %[xa ya]=size(D)
