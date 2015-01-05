74353bbd2036e57c928df253b8c78f25f3d62929
Michael Hufschmidt <michael@hufschmidt-web.de>
2015-01-05 19:27:21 +0100

    schleife verkürzt

--- Matlab/intpolval.m
+++ Matlab/intpolval.m
@@ -17,12 +17,12 @@ if length(xst) ~= size(D)
 end
 
 %Berechne das Polynom p an Stelle x.
+p(1) = D(1,1);
+r(1) = x-xst(1);
 for i=1:length(xst)-1
-    p(1) = D(1,1);
-    r(1) = x-xst(1);
     r(i+1) = r(i)*(x-xst(i+1));
     p(i+1) = p(i) + D(i+1,i+1)*r(i);
-end   
+end
 
 %Weise y das n-te Polynom an Stelle x zu.
 y= p(length(xst));
