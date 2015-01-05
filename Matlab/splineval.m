c2edcc512f0d5426b1e258960adf02b185374bf3
Michael Hufschmidt <michael@hufschmidt-web.de>
2015-01-05 21:04:08 +0100

    Gerüst für Blatt 10

---
+++ Matlab/splineval.m
@@ -0,0 +1,14 @@
+% Uebungen zur Numerischen Mathematik, WS 2014/15
+% Blatt 10, Aufgabe 36
+%
+% Autoren:
+%   Janina Geiser, Mat Nr. 6420269
+%   Michael Hufschmidt, Mat.Nr. 6436122
+%   Farina Ohm, Mat Nr. 6314051
+%   Annika Seidel, Mat Nr. 6420536
+% Uebungsleiter:
+%   Thomas Berger <thomas.berger@uni-hamburg.de>
+%
+function [y] = splineval(x, xs, fs)
+
+end
